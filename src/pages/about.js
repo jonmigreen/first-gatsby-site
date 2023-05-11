47747346382e3bf4dbe0ade0a6bcae23e053db34
jonmigreen <jonmigreen@gmail.com>
2023-05-10 18:05:02 -0700

    Two page layout

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About Us</h1>
+            <p>Come for the pulls, stay for the party!</p>
+        </main>
+    )
+}
